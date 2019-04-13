4c065c62557965797e5db7b9cfd679c7c7c5f3f5
Dominykas Blyžė <hello@dominykas.com>
2019-04-14 01:25:02 +0300

    feat: add playground

--- bin/get-packuments.js
+++ bin/get-packuments.js
@@ -2,15 +2,11 @@
 
 'use strict';
 
-const Fs = require('fs');
-const Mkdirp = require('mkdirp');
-const Pacote = require('pacote');
-const Path = require('path');
+
+const Packuments = require('../lib/packuments');
 
 
-const internals = {
-    cachePath: Path.join(__dirname, '..', 'cache', 'packuments')
-};
+const internals = {};
 
 
 internals.main = async () => {
@@ -18,24 +14,7 @@ internals.main = async () => {
     const allDeps = Object.keys(require('../package.json').dependencies).sort();
     for (const dep of allDeps) {
 
-        const parts = dep.split('/');
-
-        if (parts.length > 2) {
-            throw new Error(`What is this sourcery? Weird package name: ${dep}`);
-        }
-
-        if (parts.length === 2) {
-            Mkdirp.sync(Path.join(internals.cachePath, parts[0]));
-        }
-
-        console.log(dep);
-        const packument = await Pacote.packument(dep, {
-            'full-metadata': true,
-            'include-deprecated': true,
-            'user-agent': 'dominykas/analyze-npm-top-1000@0.0.0, see https://github.com/dominykas/analyze-npm-top-1000'
-        });
-
-        Fs.writeFileSync(Path.join(internals.cachePath, `${dep}.json`), JSON.stringify(packument, null, '  '));
+        await Packuments.download(dep);
     }
 };
 
---
+++ bin/playground.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+
+'use strict';
+
+
+const _ = require('lodash');
+const Stats = require('stats-lite');
+
+
+const Packuments = require('../lib/packuments');
+
+
+const internals = {
+    now: Date.now(),
+    dayLength: 86400 * 1000,
+    daysSince: (date) => (internals.now - date) / internals.dayLength
+};
+
+
+internals.main = async () => {
+
+    const deps = require('../package.json').dependencies;
+
+    const all = await Packuments.loadAll(deps);
+
+    const res = _(all)
+        .map(({ name, time }) => ({
+            name,
+            time: new Date(_(time).omit('modified', 'created').values().max())
+        }))
+        .map(({ time }) => internals.daysSince(time))
+        .sortBy()
+        .value();
+
+    const p = 5;
+    for (let i = 1; i <= 100 / p; ++i) {
+        const percentile = i * p;
+        const age = Stats.percentile(res, percentile / 100);
+        console.log(percentile, Math.round(age));
+    }
+};
+
+internals.main().catch((err) => {
+
+    console.error(err);
+    process.exit(1);
+});
