98b54d3fcce0e27faae383692f214b0d13f9ede4
Dominykas Blyžė <hello@dominykas.com>
2019-04-14 00:21:56 +0300

    refactor: cleanup

---
+++ bin/write-clone-sh.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const GitInfo = require('../lib/gitInfo');
+
+const Fs = require('fs');
+const Path = require('path');
+
+
+const internals = {};
+
+
+internals.main = () => {
+
+    const repoInfo = GitInfo.get(require('../package.json').dependencies);
+
+    Fs.writeFileSync(Path.join(__dirname, '..', 'cache', 'clone.sh'), repoInfo.map(({ url, gitFullName }) => `git clone ${url} ${gitFullName}`).join('\n'));
+};
+
+internals.main();
