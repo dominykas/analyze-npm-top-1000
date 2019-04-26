309a607ef1c34aaa9232f9e054fd528a656fcd15
Dominykas Blyžė <hello@dominykas.com>
2019-04-26 23:46:30 +0300

    feat: add a pull script

--- bin/write-clone-sh.js
+++ bin/write-clone-sh.js
@@ -16,6 +16,7 @@ internals.main = () => {
     const repoInfo = GitInfo.get(require('../package.json').dependencies);
 
     Fs.writeFileSync(Path.join(__dirname, '..', 'cache', 'clone.sh'), repoInfo.map(({ url, gitFullName }) => `git clone ${url} git/${gitFullName}`).join('\n'));
+    Fs.writeFileSync(Path.join(__dirname, '..', 'cache', 'pull.sh'), repoInfo.map(({ url, gitFullName }) => `git --git-dir=git/${gitFullName}/.git --work-tree=git/${gitFullName}/ pull`).join('\n'));
 };
 
 internals.main();
